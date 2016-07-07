538ef3e1a34989b8ada5f9c036333e80df6ff6f1
paul <paul@wizmacau.com>
2016-07-07 13:12:38 +0800

    format

--- pkg/common/util.go
+++ pkg/common/util.go
@@ -13,17 +13,17 @@ import (
 const (
 	//ERROR_MSG_API_FETCH = "request api timeout"
 	//ERROR_MSG_API_MISS_MATCHED = "no api matched"
-	ERROR_MSG_API_TIMEOUT = "request api timeout"
-	ERROR_MSG_JSON_ERROR = "json parse error"
+	ERROR_MSG_API_TIMEOUT        = "request api timeout"
+	ERROR_MSG_JSON_ERROR         = "json parse error"
 	ERROR_MSG_WX_POSTS_API_FETCH = "weixin fetch api error"
 	ERROR_MSG_REGEX_MISS_MATCHED = "regex miss matched"
-	ERROR_MSG_URL_NOT_MATCHED = "Url not matched"
+	ERROR_MSG_URL_NOT_MATCHED    = "Url not matched"
 	//ERROR_MSG_TIMEOUT = "request timeout"
 	//ERROR_MSG_URL_MISS_MATCHED = "url miss matched"
 )
 
 const (
-	TIMESTAMP_LAYOUT = "2006-01-02T15:04:05+0000"
+	TIMESTAMP_LAYOUT    = "2006-01-02T15:04:05+0000"
 	TIMESTAMP_LAYOUT_WB = "2006-1-2 15:4"
 )
 
@@ -103,7 +103,7 @@ func ParseWBCreatedAt(dateStr string) int64 {
 	today := time.Now()
 	var resultStr string
 	if strings.Contains(dateStr, "分钟前") {
-		mAgo := Str2Int(dateStr[:len(dateStr) - 9])
+		mAgo := Str2Int(dateStr[:len(dateStr)-9])
 		var h int
 		var m int
 		if today.Minute() >= mAgo {
@@ -115,7 +115,7 @@ func ParseWBCreatedAt(dateStr string) int64 {
 		}
 		resultStr = fmt.Sprint(today.Year(), "-", int(today.Month()), "-", today.Day(), " ", h, ":", m)
 	} else if strings.Contains(dateStr, "今天") {
-		resultStr = fmt.Sprint(today.Year(), "-", int(today.Month()), "-", today.Day(), " ", dateStr[len(dateStr) - 5:len(dateStr)])
+		resultStr = fmt.Sprint(today.Year(), "-", int(today.Month()), "-", today.Day(), " ", dateStr[len(dateStr)-5:len(dateStr)])
 	} else {
 		resultStr = fmt.Sprint(today.Year(), "-", dateStr)
 	}
