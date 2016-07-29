2f791f1455444e6f8ab0277b2cea9e94ddbb95e7
paul <paul@wizmacau.com>
2016-07-29 12:03:38 +0800

    add template array test

--- pkg/controllers/web.go
+++ pkg/controllers/web.go
@@ -7,7 +7,12 @@ import (
 )
 
 func WEB(c *gin.Context)  {
+	a := make([]string, 5)
+
 	c.HTML(http.StatusOK, "api.templ.html", gin.H{
 		"timestamp": time.Now().Unix(),
+		"array":a,
 	})
 }
+
+var array_data = ``
